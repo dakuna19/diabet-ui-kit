ad48d2772a7fd3e935a6926e53fad9395bb3e701
alexey <macbook@mac-mini-alexey>
2016-12-08 12:46:11 +0200

    Initial commit

---
+++ diabetUIKit/APMessagesVC.swift
@@ -0,0 +1,85 @@
+//
+//  APMessagesVC.swift
+//  diabet
+//
+//  Created by alexeyweb39@gmail.com on 08/12/2016, using AnimaApp.com, under MIT license.
+//  Copyright © 2016 Company Name. All rights reserved.
+//
+
+import UIKit
+
+public class APMessagesVC : ANViewController {
+    
+    @IBOutlet weak var messages: UIView?
+    @IBOutlet weak var messages9: UIView?
+    @IBOutlet weak var messages9Line: UIView?
+    @IBOutlet weak var messages9OurStartupSources: UIImageView?
+    @IBOutlet weak var messages9Icon: UIImageView?
+    @IBOutlet weak var messages9Yesterday: UIImageView?
+    @IBOutlet weak var messages9JeaneChenier: UIImageView?
+    @IBOutlet weak var messages9Userpic: UIImageView?
+    @IBOutlet weak var messages8: UIView?
+    @IBOutlet weak var messages8Line: UIView?
+    @IBOutlet weak var messages8SpeakingOfWhichP: UIImageView?
+    @IBOutlet weak var messages8Icon: UIImageView?
+    @IBOutlet weak var messages8Yesterday: UIImageView?
+    @IBOutlet weak var messages8MarkusBradway: UIImageView?
+    @IBOutlet weak var messages8Userpic: UIImageView?
+    @IBOutlet weak var messages7: UIView?
+    @IBOutlet weak var messages7Line: UIView?
+    @IBOutlet weak var messages7ThatsClear: UIImageView?
+    @IBOutlet weak var messages7Icon: UIImageView?
+    @IBOutlet weak var messages7Yesterday: UIImageView?
+    @IBOutlet weak var messages7TalithaStinger: UIImageView?
+    @IBOutlet weak var messages7Userpic: UIImageView?
+    @IBOutlet weak var messages6: UIView?
+    @IBOutlet weak var messages6Line: UIView?
+    @IBOutlet weak var messages6Icon: UIImageView?
+    @IBOutlet weak var messages6Yesterday: UIImageView?
+    @IBOutlet weak var messages6BecauseTheWholeHo: UIImageView?
+    @IBOutlet weak var messages6EhtelWolfram: UIImageView?
+    @IBOutlet weak var messages6Userpic: UIImageView?
+    @IBOutlet weak var messages5: UIView?
+    @IBOutlet weak var messages5Line: UIView?
+    @IBOutlet weak var messages5IThinkItsBecause: UIImageView?
+    @IBOutlet weak var messages5Icon: UIImageView?
+    @IBOutlet weak var messages5Yesterday: UIImageView?
+    @IBOutlet weak var messages5JackAhlers: UIImageView?
+    @IBOutlet weak var messages5Userpic: UIImageView?
+    @IBOutlet weak var messages4: UIView?
+    @IBOutlet weak var messages4Line: UIView?
+    @IBOutlet weak var messages4WhyAreThereSoMan: UIImageView?
+    @IBOutlet weak var messages4Icon: UIImageView?
+    @IBOutlet weak var messages4Yesterday: UIImageView?
+    @IBOutlet weak var messages4BillieJanzen: UIImageView?
+    @IBOutlet weak var messages4UserpicIcon: UIImageView?
+    @IBOutlet weak var messages3: UIView?
+    @IBOutlet weak var messages3Line: UIView?
+    @IBOutlet weak var messages3BothOfThem: UIImageView?
+    @IBOutlet weak var messages3Icon: UIImageView?
+    @IBOutlet weak var messages3Yesterday: UIImageView?
+    @IBOutlet weak var messages3KarlNyland: UIImageView?
+    @IBOutlet weak var messages3Userpic: UIImageView?
+    @IBOutlet weak var messages2: UIView?
+    @IBOutlet weak var messages2Line: UIView?
+    @IBOutlet weak var messages2YoureOneOfPeter: UIImageView?
+    @IBOutlet weak var messages2Icon: UIImageView?
+    @IBOutlet weak var messages2Yesterday: UIImageView?
+    @IBOutlet weak var messages2JodeeFurrow: UIImageView?
+    @IBOutlet weak var messages2Userpic: UIImageView?
+    @IBOutlet weak var messages1: UIView?
+    @IBOutlet weak var messages1Line: UIView?
+    @IBOutlet weak var messages1JaredsBeenBackFr: UIImageView?
+    @IBOutlet weak var messages1Icon: UIImageView?
+    @IBOutlet weak var messages1Yesterday: UIImageView?
+    @IBOutlet weak var messages1AnnieHall: UIImageView?
+    @IBOutlet weak var messages1Userpic: UIImageView?
+    @IBOutlet weak var topBar: UIView?
+    @IBOutlet weak var topBarBackground: UIImageView?
+    @IBOutlet weak var topBarNewMessageIcon: UIImageView?
+    @IBOutlet weak var topBarMessages: UIImageView?
+    @IBOutlet weak var topBarEdit: UIImageView?
+
+    
+
+}
---
+++ diabetUIKit/diabetUIKit.h
@@ -0,0 +1,39 @@
+//
+//  diabetUIKit.h
+//  diabetUIKit
+//
+//  Created by alexeyweb39@gmail.com on 08/12/2016, using AnimaApp.com, under MIT license.
+//  Copyright © 2016 Company Name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for diabetUIKit.
+FOUNDATION_EXPORT double diabetUIKitVersionNumber;
+
+//! Project version string for diabetUIKit.
+FOUNDATION_EXPORT const unsigned char diabetUIKitVersionString[];
+
+#import <diabetUIKit/Anima-Bridging-Header.h>
+#import <diabetUIKit/ANGradientView.h>
+#import <diabetUIKit/ANNavigationController.h>
+#import <diabetUIKit/UIWebView+Anima.h>
+#import <diabetUIKit/ANTabBarController.h>
+#import <diabetUIKit/UIView+AnimaConstraints.h>
+#import <diabetUIKit/AnimaSDK.h>
+#import <diabetUIKit/ANViewController.h>
+#import <diabetUIKit/UIImageView+Anima.h>
+#import <diabetUIKit/ANVideoPlayer.h>
+#import <diabetUIKit/ANCSVParser.h>
+#import <diabetUIKit/NSDictionary+Anima.h>
+#import <diabetUIKit/ANTheme.h>
+#import <diabetUIKit/ANDataList.h>
+#import <diabetUIKit/NSString+Anima.h>
+#import <diabetUIKit/MKMapView+Anima.h>
+#import <diabetUIKit/ANTextField.h>
+#import <diabetUIKit/ANProtocols.h>
+#import <diabetUIKit/ANLabel.h>
+#import <diabetUIKit/ANRSSParser.h>
+#import <diabetUIKit/ANMacros.h>
+#import <diabetUIKit/UIView+Anima.h>
+#import <diabetUIKit/ANClassMapping.h>
